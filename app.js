7ddd43796491b3eabf2ceb4d6b0e11c102b3bf17
Tony Elmore <tony@tonyelmore.com>
2024-10-09 14:24:50 -0500

    add curl to userinfo

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const jwt = require('jsonwebtoken');
 const https = require('https');
 const crypto = require('crypto');
 const { v4: uuidv4 } = require('uuid');
+const ldap = require('ldapjs');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -27,7 +28,7 @@ const credhubCredentials = vcapServices.credhub[0].credentials;
 const mySecret = credhubCredentials.keyone;
 
 //LDAP
-const hardcodedEmail = 'johndoe@example.org';
+//const hardcodedEmail = 'tonyelmore@example.org';
 const developersGroupDN = 'cn=developers, dc=example, dc=org';
 const userSearchDN = 'dc=example,dc=org';
 const adminDN = 'cn=admin,dc=example,dc=org';
@@ -39,7 +40,7 @@ const agent = new https.Agent({
     rejectUnauthorized: false,
 });
 
-app.get('/secure-endpoint', async (req, res) => {    
+app.get('/', async (req, res) => {    
 
     // ##### Encrypt and Decrypt Data #####
 
@@ -78,32 +79,127 @@ app.get('/secure-endpoint', async (req, res) => {
 
 
 
-
     // Back to our normally scheduled programming
     const wftoken = req.headers['wftoken'];
     if (!wftoken) {
         return res.status(400).send('No token provided, only got $req.headers');
     }
-
+    
     // Decode the JWT token and get the userID
     try {
+        // Don't even treally need to get the userid from the token because the call to uaa userinfo takes in the token 
+        // and returns the user email field
         // Decode the JWT token to extract user_id
         const decodedToken = jwt.decode(wftoken);
         if (!decodedToken || !decodedToken.user_id) {
             return res.status(400).json({ error: 'Invalid token or user_id not found'});
         }
 
-        const userID = decodedToken.user_id;
+        // const userID = decodedToken.user_id;
+        // console.log('userID extracted from token: ', userID);
 
+
+
+
+        // #### Get UserId by curl to uaa ####
+        const userInfoUrl = `https://uaa.sys.${systemDomain}.h2o.vmware.com/userinfo`;
+        console.log("userurl: ", userInfoUrl);
+        console.log("token: ", wftoken);
+        const userInfoResponse = await axios.get(userInfoUrl, {
+            headers: {
+                Authorization: `bearer ${wftoken}`
+            },
+            httpsAgent: agent,
+        });
+
+        console.log("Return from call", userInfoResponse.data);
+        let userID = userInfoResponse.data.email;
+        console.log('User name: ', userID);
+
+
+        // Test - should have "admin"
+        // Test - change to hardcoded value just to hit LDAP correctly
+        userID = 'tonyelmore@example.org';
+
+
+
+
+
+        // #### Find user in LDAP ####
         // Using the user_id extracted from the token, check if that user is in LDAP
         // and if they belong to the correct group.
+        const client = ldap.createClient({ url: ldapUrl });
+
+        client.bind(adminDN, adminPassword, (err) => {
+            if (err) {
+                return res.status(500).json({ message: 'LDAP bind failed', error: err });
+            }
+            const searchFilter = `(mail=${userID})`;
+            const searchOptions = {
+                filter: searchFilter,
+                scope: 'sub',
+                attributes: ['dn', 'cn'],   // Retrieve distinguished name and common name
+            };
+    
+            client.search(userSearchDN, searchOptions, (err, result) => {
+                if (err) {
+                    return res.status(500).json({ message: 'LDAP Search Failed', error: err });
+                }
+    
+                let userDN = null;
+    
+                result.on('searchEntry', (entry) => {
+                    userDN = entry.object.dn;
+                });
+                result.on('end', () => {
+                    if (!userDN) {
+                        client.unbind();
+                        return res.status(403).json({ message: 'User not found' });
+                    }
+    
+                    const groupFilter = `(member=${userDN})`;
+                    const groupSearchOptions = {
+                        filter: groupFilter,
+                        scope: 'sub',
+                        attributes: ['cn']   // We only care if the group entry exists
+                    };
+    
+                    client.search(developersGroupDN, groupSearchOptions, (err, groupResult) => {
+                        if (err) {
+                            client.unbind();
+                            return res.status(500).json({ message: 'LDAP Group Search Failed', error: err });
+                        }
+                        let isMember = false;
+                        groupResult.on('searchEntry', () => {
+                            isMember = true;    // If we find the user in the group
+                        });
+    
+                        groupResult.on('end', () => {
+                            client.unbind();  // Close the connection
+                            if (isMember) {
+                                    return res.status(200).json({ message: 'User is in the group - return the ssh key', userID });
+                            } else {
+                                    return res.status(403).json({ message: 'User is not in the group - do not return the ssh key', userID });
+                            }
+                        });
+                    });
+                });
+            });
+        });
+        // #### End LDAP ####
 
 
-
-        res.json({ result: hasOrg });
+        // res.status(500).json({ result: 'Unknown error - should not have gotten this far' });
         // res.send(`${mySecret}`);
+
     } catch (error) {
-        console.error('Error:', error.message);
+        if (error.response) {
+            console.error('Error Response Data', error.response.data);
+            console.error('Error Status: ', error.response.status);
+            console.error('Error Headers:', error.response.headers);
+        } else {
+            console.error('Error:', error.message);
+        }
         res.status(500).json({ error: 'An error occured while processing the request' });
     }
 
@@ -113,7 +209,7 @@ app.get('/secure-endpoint', async (req, res) => {
 
 
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     let credentials;
 
     if (process.env.VCAP_SERVICES) {
